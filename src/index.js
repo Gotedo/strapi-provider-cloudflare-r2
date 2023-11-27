cb0f8c14576de85172498a096bede59a5bde9f49
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2023-11-27 19:28:14 +0100

    chore: setup
    - Relax Nodejs version requirement
    - Update package.json properties
    - Rename `lib` to `src`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Module dependencies
@@ -6,26 +6,26 @@
 
 /* eslint-disable no-unused-vars */
 // Public node modules.
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 module.exports = {
   init(config) {
     const S3 = new AWS.S3({
       endpoint: `https://${config.params.accountId}.r2.cloudflarestorage.com`,
-      signatureVersion: 'v4',
-      ...config,
+      signatureVersion: "v4",
+      ...config
     });
 
     const upload = (file, customParams = {}) =>
       new Promise((resolve, reject) => {
         // upload file on S3 bucket
-        const path = file.path ? `${file.path}/` : '';
+        const path = file.path ? `${file.path}/` : "";
         S3.upload(
           {
             Key: `${path}${file.hash}${file.ext}`,
-            Body: file.stream || Buffer.from(file.buffer, 'binary'),
+            Body: file.stream || Buffer.from(file.buffer, "binary"),
             ContentType: file.mime,
-            ...customParams,
+            ...customParams
           },
           (err, data) => {
             if (err) {
@@ -34,7 +34,7 @@ module.exports = {
 
             // set the bucket file url
             file.url = `${config.params.publicUrl}/${data.Key}`;
-            
+
             resolve();
           }
         );
@@ -50,11 +50,11 @@ module.exports = {
       delete(file, customParams = {}) {
         return new Promise((resolve, reject) => {
           // delete file on S3 bucket
-          const path = file.path ? `${file.path}/` : '';
+          const path = file.path ? `${file.path}/` : "";
           S3.deleteObject(
             {
               Key: `${path}${file.hash}${file.ext}`,
-              ...customParams,
+              ...customParams
             },
             (err, data) => {
               if (err) {
@@ -65,7 +65,7 @@ module.exports = {
             }
           );
         });
-      },
+      }
     };
-  },
+  }
 };
